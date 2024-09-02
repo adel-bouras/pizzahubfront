46bc459333598e9dd19becc2fe3c38fcbb55bfc0
adel-bouras <eyyad.kareem1212@gmail.com>
2024-09-02 14:47:35 +0100

    done

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server : {
     port : 3000
+  }, build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
